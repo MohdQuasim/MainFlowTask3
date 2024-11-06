69f33a83c331a9c06a224434dfb72e7057c0b016
MohdQuasim <quasim.ibm1981@gmail.com>
2024-11-06 20:03:37 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,41 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "1808";
+$dbname = "loginSystem";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    // Validate passwords match
+    if ($password === $confirm_password) {
+        // Hash the password for security
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+        // Insert user into the database
+        $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Signup successful!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else {
+        echo "Passwords do not match!";
+    }
+}
+
+$conn->close();
+?>
